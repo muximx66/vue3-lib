5eebac5d5ff6a5af9a3c5376c889f224b948a7cb
muxi66 <muximx66@163.com>
2024-02-03 10:10:55 +0800

    update:ctx-menu-panel

---
+++ src/demo/ctx-menu-panel/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :[triggerAttr]="item" v-for="(item, index) in data" :key="index">{{ item }}</div>
+  <CtxMenuPanel :data="menuData">
+    <template #default>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+      <div>泥嚎</div>
+    </template>
+  </CtxMenuPanel>
+</template>
+<script setup lang="tsx">
+import { ref } from 'vue'
+import CtxMenuPanel, { triggerAttr } from '@/packages/ctx-menu-panel/index'
+
+
+const menuData = ref<Map<string, any>>(new Map());
+
+const data = ["12312", "fewfwefwef", "fwefwe", 'wfr23fwe']
+
+data.forEach(v => {
+  menuData.value.set(v, v)
+})
+
+</script>
+<style scoped lang="less"></style>
