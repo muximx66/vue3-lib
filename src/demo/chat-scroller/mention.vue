59617911a4914f857bbc1503ece1cb88afd9d9c9
zouxinhong <muximx66@163.com>
2024-02-21 16:58:05 +0800

    update:mention

--- src/demo/chat-scroller/mention.vue
+++ src/demo/chat-scroller/mention.vue
@@ -8,25 +8,25 @@
   </div>
 </template>
 <script setup lang="tsx">
-import { computed, shallowRef } from 'vue';
+import { computed, shallowRef } from "vue";
+
 type Props = {
   rect?: DOMRect;
-}
+};
 const props = defineProps<Props>();
 
 const mention = shallowRef<HTMLDivElement>();
 
 const styles = computed(() => {
   const rect = props?.rect;
-  const top = rect?.top || 0
+  const top = rect?.top || 0;
   const left = rect?.left || 0;
   const clientHeight = window.innerHeight || document.body.offsetHeight || 0;
   return {
-    left: left + 'px',
-    bottom: clientHeight - top + 'px'
-  }
-})
-
+    left: left + "px",
+    bottom: clientHeight - top + "px",
+  };
+});
 </script>
 <style scoped lang="less">
 .mention {
